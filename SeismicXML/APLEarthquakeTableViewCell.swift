d138ecdbebde4ee3295c92ec6a3f2c660f1e71df
Monir <goblortikus-github@yahoo.com>
2015-05-06 20:40:58 -0700

    created initial swift class and bridging header

---
+++ SeismicXML/APLEarthquakeTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  APLEarthquakeTableViewCell.swift
+//  SeismicXML
+//
+//  Created by MM on 5/6/15.
+//  Copyright (c) 2015 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class APLEarthquakeTableViewCell: UITableViewCell {
+
+}
